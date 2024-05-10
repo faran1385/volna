2ae61802bb3308344fbcd5980516aedfa16ca7e0
matinab2llahi <m4451657@gmail.com>
2024-05-10 14:52:27 +0330

    responsive header

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -8,7 +8,7 @@ export const Header: React.FC = () => {
     return (
         <div className='Header-nav flex justify-end xl:static fixed top-0 w-full  '>
             <div className='Header-nav__box--xl relative  transition-all duration-150 flex xl:justify-end justify-end items-center '>
-                <header className="xl:hidden absolute left-3 p-5 flex items-center  sidebar__header">
+                <header className={`xl:hidden ${toggleSearch?"absolute -translate-y-24":""} transition-all duration-300 absolute left-3 p-5 flex items-center  sidebar__header`}>
                     <img className="h-full sidebar__header__image" src="https://volna.volkovdesign.com/img/logo.svg" title={'logo'} />
                 </header>
                 <div className='Header-nav__left w-auto Header-nav__left--action   absolute  flex items-center ps-7'>
@@ -17,14 +17,14 @@ export const Header: React.FC = () => {
                         <a href="#" className='capitalize me-7'>about</a>
                         <a href="#" className='capitalize'>contacts</a>
                     </nav>
-                    <div style={{ backgroundColor: "var(--background)" }} className={`xl:w-auto xl:static  transition-all duration-500 flex gap-2 fixed z-40 w-full ${toggleSearch ? "top-4 " : "-top-36"}`}>
+                    <div style={{ backgroundColor: "var(--background)" }} className={`xl:w-auto xl:static Header-nav__left__inputbox--resize   transition-all duration-500 flex justify-between xl:gap-2 fixed z-40 w-11/12 ${toggleSearch ? "top-4  " : "-top-36 "}`}>
                         <SearchInput />
                         <button className='xl:hidden block' title='cluse' onClick={() => setToggleSearch(false)}>
                             <svg className='Header-nav__controler__links__icon ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></svg>
                         </button>
                     </div>
                 </div>
-                <div className='Header-nav__controler pe-7 flex items-center'>
+                <div className={`Header-nav__controler ${toggleSearch?"absolute -translate-y-24":""} transition-all duration-300 pe-7 flex items-center`}>
                     <div className='Header-nav__controler__links sidebar__nav__link flex gap-7 pe-8 '>
                         <button onClick={() => setToggleSearch(!toggleSearch)} className='xl:hidden block' title='search'>
                             <svg className='Header-nav__controler__links__icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"></path></svg>
@@ -48,7 +48,7 @@ export const Header: React.FC = () => {
                             </DropDown>
                         </div>
                     </div>
-                    <a href="#" className="flex ps-8 w-fit sidebar__nav__link">
+                    <a href="#" className="flex ps-8 w-fit Header-nav__controler__signin sidebar__nav__link">
                         <span className=" sidebar__nav__link__text capitalize">sign in</span>
                         <svg className="sidebar__nav__link__icon ms-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path d="M20,12a1,1,0,0,0-1-1H11.41l2.3-2.29a1,1,0,1,0-1.42-1.42l-4,4a1,1,0,0,0-.21.33,1,1,0,0,0,0,.76,1,1,0,0,0,.21.33l4,4a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42L11.41,13H19A1,1,0,0,0,20,12ZM17,2H7A3,3,0,0,0,4,5V19a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V16a1,1,0,0,0-2,0v3a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V8a1,1,0,0,0,2,0V5A3,3,0,0,0,17,2Z"></path></svg>
@@ -70,7 +70,7 @@ export const Header: React.FC = () => {
 
 const SearchInput: React.FC = () => {
     return (
-        <div className='Header-nav__searech flex justify-between py-2  px-5 items-center w-11/12  gap-2'>
+        <div className='Header-nav__searech xl:w-full flex flex-row justify-between py-2  px-5 items-center  gap-2'>
             <input className='w-11/12' type="text" title='search' placeholder='Artist, track or podcast' />
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"></path></svg>
         </div>
