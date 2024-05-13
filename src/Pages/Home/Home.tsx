82d26676ea7850a14c3da3ebff54cbdaeea08f47
faran <hossinifaran@gmail.com>
2024-05-13 11:23:56 +0330

    layout bug fixed

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,9 @@
 import "./Home.css"
 export const Home = () => {
-    return <main className="home w-full">
+    return <main className="home home--mobile w-full">
+        dgfdg
+        <div className="w-full h-screen"></div>
+        <div className="w-full h-screen"></div>
+        <div className="w-full h-screen"></div>
     </main>
 }
