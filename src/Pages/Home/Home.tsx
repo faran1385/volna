2893b166c0fb0075b832c141998bb403eea80e47
matinab2llahi <m4451657@gmail.com>
2024-05-14 00:38:04 +0330

    change import slider

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import { Releases } from "../../components/ui/NewReleases/Releases"
 import { Slider } from "../../components/ui/Slider/Slider"
 import "./Home.css"
-import { Slider } from "../../components/ui/Slider/Slider"
 export const Home = () => {
     return <main className="home home--mobile w-full">
         <Slider/>
