a9fc50d7a11fc78478d240f80d1ab27d202f2893
faran <hossinifaran@gmail.com>
2024-05-13 10:39:59 +0330

    setting up the home component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react"
 import { Player } from "./components/ui/Player/Player"
 import { Sidebar } from "./components/ui/Sidebar/Sidebar"
 import { Header } from "./components/ui/header/Header"
+import { Home } from "./components/ui/Home/Home"
 export interface StateMenuType {
   toggleMenu: boolean | null,
   setToggleMenu: (value: boolean) => void
@@ -11,13 +12,12 @@ function App() {
   const [toggleMenu, setToggleMenu] = useState<boolean>(false)
   return (
     <>
-      <ContextMenu.Provider value={{toggleMenu, setToggleMenu}}>
-        <main>
-          <Sidebar />
-          <Header/>
-          <Player/>
-        </main>
+      <ContextMenu.Provider value={{ toggleMenu, setToggleMenu }}>
+        <Sidebar />
+        <Header />
       </ContextMenu.Provider>
+      <Player />
+      <Home />
     </>
   )
 }
---
+++ src/components/ui/Home/Home.tsx
@@ -0,0 +1,5 @@
+import "./Home.css"
+export const Home = () => {
+    return <main className="home w-full">
+    </main>
+}
