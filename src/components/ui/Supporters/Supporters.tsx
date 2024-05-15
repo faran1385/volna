1acd1d709d078b7163bd2c00d4c445079f057fa0
matinab2llahi <m4451657@gmail.com>
2024-05-15 21:29:43 +0330

    change some Supporters

--- src/components/ui/Supporters/Supporters.tsx
+++ src/components/ui/Supporters/Supporters.tsx
@@ -6,24 +6,25 @@ export const Supporters : React.FC = () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const nameSlider = 'supporters';
     const swiperRef = useRef<any>(null)
-    const [slidesPerView, setSlides] = useState(window.innerWidth < 800 ? 4 : 2)
     return (
         <div className='supporters px-8 my-9'>
             <Swiper
                 modules={[Autoplay]}
                 autoplay={{pauseOnMouseEnter:true,delay:5000}}
                 speed={1000}
-                slidesPerView={4}
+                slidesPerView={6}
                 loop={true}
-                onBreakpoint={() => setSlides(window.innerWidth < 800 ? 4 : 2)}
                 spaceBetween={30}
                 ref={swiperRef}
                 breakpoints={{
                     0: {
                         slidesPerView: 2
                     },
-                    800: {
+                    650: {
                         slidesPerView: 4
+                    },
+                    1024: {
+                        slidesPerView: 6
                     }
                 }}
                 className={`my-4 ${nameSlider}-slider`}
@@ -31,6 +32,36 @@ export const Supporters : React.FC = () => {
                 <SwiperSlide>
                     <SupportersItems
                         srcImg='http://volna.volkovdesign.com/img/partners/codecanyon-light-background.png'
+                        href='#'
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <SupportersItems
+                        srcImg='http://volna.volkovdesign.com/img/partners/photodune-light-background.png'
+                        href='#'
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <SupportersItems
+                        srcImg='http://volna.volkovdesign.com/img/partners/3docean-light-background.png'
+                        href='#'
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <SupportersItems
+                        srcImg='http://volna.volkovdesign.com/img/partners/themeforest-light-background.png'
+                        href='#'
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <SupportersItems
+                        srcImg='http://volna.volkovdesign.com/img/partners/activeden-light-background.png'
+                        href='#'
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <SupportersItems
+                        srcImg='http://volna.volkovdesign.com/img/partners/audiojungle-light-background.png'
                         href='#'
                     />
                 </SwiperSlide>
