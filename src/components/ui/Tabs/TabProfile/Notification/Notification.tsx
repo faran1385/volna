29d6dedd645ea189df50b79792a5798b0d93b42c
faran <hossinifaran@gmail.com>
2024-06-01 22:12:26 +0330

    profile tab ui added

---
+++ src/components/ui/Tabs/TabProfile/Notification/Notification.tsx
@@ -0,0 +1,58 @@
+import "./Notification.css"
+import React from "react";
+
+interface NotificationProps {
+    title: string,
+    link: string,
+    type: "success" | "failed" | "info" | "gift",
+    time: number,
+    href: string,
+    hasBorder: boolean
+}
+
+export const Notification: React.FC<NotificationProps> = (T) => {
+
+    const {href, hasBorder, time, title, type, link} = T
+
+    const passedTime = `${Math.floor(time / 24) !== 0 ? Math.floor(time / 24) : ""} ${Math.floor(time / 24) !== 0 ? "Day" : ""} ${Math.floor(time / 24) === 0 || Math.floor(time % 24) === 0 ? "" : "and "} ${time % 24 !== 0 ? Math.floor(time % 24) : ""} ${time % 24 !== 0 ? "Hours" : ""} ago`
+
+    return <>
+        <div className={"w-full"}>
+            <div className={"py-2"} style={{borderBottom: hasBorder ? '1px solid var(--border-color)' : ""}}>
+                <div className={"flex items-center"}>
+                    {type === "success" ? (
+                        <svg className={"me-2"} fill={"var(--green-color)"} xmlns="http://www.w3.org/2000/svg"
+                             width={20} viewBox="0 0 24 24">
+                            <path
+                                d="M14.72,8.79l-4.29,4.3L8.78,11.44a1,1,0,1,0-1.41,1.41l2.35,2.36a1,1,0,0,0,.71.29,1,1,0,0,0,.7-.29l5-5a1,1,0,0,0,0-1.42A1,1,0,0,0,14.72,8.79ZM12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z">
+                            </path>
+                        </svg>) : type === "failed" ? (
+                        <svg className={"me-2"} xmlns="http://www.w3.org/2000/svg" fill={"#eb5757"} width={20}
+                             viewBox="0 0 24 24">
+                            <path
+                                d="M15.71,8.29a1,1,0,0,0-1.42,0L12,10.59,9.71,8.29A1,1,0,0,0,8.29,9.71L10.59,12l-2.3,2.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42L13.41,12l2.3-2.29A1,1,0,0,0,15.71,8.29Zm3.36-3.36A10,10,0,1,0,4.93,19.07,10,10,0,1,0,19.07,4.93ZM17.66,17.66A8,8,0,1,1,20,12,7.95,7.95,0,0,1,17.66,17.66Z"></path>
+                        </svg>
+                    ) : type === "info" ? (
+                        <svg className={"me-2"} width={20} xmlns="http://www.w3.org/2000/svg" fill={"var(--gray-color)"}
+                             viewBox="0 0 24 24">
+                            <path
+                                d="M12,2A10,10,0,1,0,22,12,10.01114,10.01114,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8.00917,8.00917,0,0,1,12,20Zm0-8.5a1,1,0,0,0-1,1v3a1,1,0,0,0,2,0v-3A1,1,0,0,0,12,11.5Zm0-4a1.25,1.25,0,1,0,1.25,1.25A1.25,1.25,0,0,0,12,7.5Z"></path>
+                        </svg>
+                    ) : type === "gift" ? (
+                        <svg className={"me-2"} xmlns="http://www.w3.org/2000/svg" width={20} fill={"#8051d4"}
+                             viewBox="0 0 24 24">
+                            <path
+                                d="M18,7h-.35A3.45,3.45,0,0,0,18,5.5a3.49,3.49,0,0,0-6-2.44A3.49,3.49,0,0,0,6,5.5,3.45,3.45,0,0,0,6.35,7H6a3,3,0,0,0-3,3v2a1,1,0,0,0,1,1H5v6a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V13h1a1,1,0,0,0,1-1V10A3,3,0,0,0,18,7ZM11,20H8a1,1,0,0,1-1-1V13h4Zm0-9H5V10A1,1,0,0,1,6,9h5Zm0-4H9.5A1.5,1.5,0,1,1,11,5.5Zm2-1.5A1.5,1.5,0,1,1,14.5,7H13ZM17,19a1,1,0,0,1-1,1H13V13h4Zm2-8H13V9h5a1,1,0,0,1,1,1Z">
+                            </path>
+                        </svg>
+                    ) : ""}
+                    <p className={"notification__text"}>
+                        <a className={"notification__link"} href={href}>{link}</a>
+                        {title}
+                    </p>
+                </div>
+                <p className={"mt-2 notification__time"}>{passedTime}</p>
+            </div>
+        </div>
+    </>
+}
--- src/components/ui/Tabs/TabProfile/TabProfile.tsx
+++ src/components/ui/Tabs/TabProfile/TabProfile.tsx
@@ -1,5 +1,7 @@
 import "./TabProfile.css"
 import React, {useRef} from "react";
+import {Notification} from "./Notification/Notification.tsx";
+import {Song} from "../../Songs/Song/Song.tsx";
 
 export const TabProfile = () => {
     const maskedValue = useRef('__-__-__-__')
@@ -51,7 +53,7 @@ export const TabProfile = () => {
     }
 
     return <div className={"mt-8"}>
-        <div className={"grid grid-cols-4 gap-6"}>
+        <div className={"grid sm:grid-cols-2  lg:grid-cols-4 gap-6"}>
             <div className={"p-4 tabs__profile__status rounded-xl"}>
                 <div className={"flex items-center"}>
                     <h1 className={"tabs__profile__status__title"}>My balance</h1>
@@ -115,7 +117,104 @@ export const TabProfile = () => {
                     <a href={"#"} className={"tabs__profile__status__button"}>Send</a>
                 </div>
             </div>
+        </div>
+        <div className={"mt-12 gap-6 grid lg:grid-cols-2"}>
+            <div className={"rounded-xl tabs__profile__container"}>
+                <div className={"h-16 tabs__profile__container__header px-4 flex justify-between items-center"}>
+                    <div className={"flex items-center"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} fill={'var(--green-color)'}
+                             viewBox="0 0 24 24">
+                            <path
+                                d="M19.05566,2h-14a3.00328,3.00328,0,0,0-3,3V19a3.00328,3.00328,0,0,0,3,3h14a3.00328,3.00328,0,0,0,3-3V5A3.00328,3.00328,0,0,0,19.05566,2Zm-14,2h14a1.001,1.001,0,0,1,1,1v8H17.59082a1.99687,1.99687,0,0,0-1.66406.89062L14.52051,16H9.59082L8.18457,13.89062A1.99687,1.99687,0,0,0,6.52051,13H4.05566V5A1.001,1.001,0,0,1,5.05566,4Zm14,16h-14a1.001,1.001,0,0,1-1-1V15H6.52051l1.40625,2.10938A1.99687,1.99687,0,0,0,9.59082,18h4.92969a1.99687,1.99687,0,0,0,1.66406-.89062L17.59082,15h2.46484v4A1.001,1.001,0,0,1,19.05566,20Z">
+                            </path>
+                        </svg>
+                        <span className={"ms-2 tabs__profile__container__header__title"}>Notifications</span>
+                        <span
+                            className={"w-4 h-4 ms-1 mb-1 flex items-center justify-center tabs__profile__container__header__badge"}>17</span>
+                    </div>
+                    <a href={"#"}>
+                        <svg className={"tabs__profile__container__header__history"} xmlns="http://www.w3.org/2000/svg"
+                             width={18} viewBox="0 0 24 24">
+                            <path
+                                d="M12,2A10,10,0,0,0,5.12,4.77V3a1,1,0,0,0-2,0V7.5a1,1,0,0,0,1,1H8.62a1,1,0,0,0,0-2H6.22A8,8,0,1,1,4,12a1,1,0,0,0-2,0A10,10,0,1,0,12,2Zm0,6a1,1,0,0,0-1,1v3a1,1,0,0,0,1,1h2a1,1,0,0,0,0-2H13V9A1,1,0,0,0,12,8Z">
 
+                            </path>
+                        </svg>
+                    </a>
+                </div>
+                <div className={"px-4 py-4"}>
+                    <div className={"overflow-y-auto"} style={{height: '350px'}}>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder={false} link={"Payment #51 "} time={4} type={"gift"}
+                                      title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder link={"Payment #51 "} time={4} type={"gift"} title={" was successful!"}
+                                      href={"#"}/>
+                        <Notification hasBorder={false} link={"Payment #51 "} time={4} type={"gift"}
+                                      title={" was successful!"}
+                                      href={"#"}/>
+                    </div>
+                </div>
+            </div>
+            <div className={"rounded-xl tabs__profile__container"}>
+                <div className={"h-16 tabs__profile__container__header px-4 flex justify-between items-center"}>
+                    <div className={"flex items-center"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} fill={'var(--green-color)'}
+                             viewBox="0 0 24 24">
+                            <path
+                                d="M21.65,2.24a1,1,0,0,0-.8-.23l-13,2A1,1,0,0,0,7,5V15.35A3.45,3.45,0,0,0,5.5,15,3.5,3.5,0,1,0,9,18.5V10.86L20,9.17v4.18A3.45,3.45,0,0,0,18.5,13,3.5,3.5,0,1,0,22,16.5V3A1,1,0,0,0,21.65,2.24ZM5.5,20A1.5,1.5,0,1,1,7,18.5,1.5,1.5,0,0,1,5.5,20Zm13-2A1.5,1.5,0,1,1,20,16.5,1.5,1.5,0,0,1,18.5,18ZM20,7.14,9,8.83v-3L20,4.17Z">
+                            </path>
+                        </svg>
+                        <span className={"ms-2 tabs__profile__container__header__title"}>New items for you</span>
+                    </div>
+                    <div className={"flex items-center"}>
+                        <a href={"#"}>
+                            <svg className={"tabs__profile__container__header__history"}
+                                 xmlns="http://www.w3.org/2000/svg"
+                                 width={18} viewBox="0 0 24 24">
+                                <path
+                                    d="M12,2A10,10,0,0,0,5.12,4.77V3a1,1,0,0,0-2,0V7.5a1,1,0,0,0,1,1H8.62a1,1,0,0,0,0-2H6.22A8,8,0,1,1,4,12a1,1,0,0,0-2,0A10,10,0,1,0,12,2Zm0,6a1,1,0,0,0-1,1v3a1,1,0,0,0,1,1h2a1,1,0,0,0,0-2H13V9A1,1,0,0,0,12,8Z">
+
+                                </path>
+                            </svg>
+                        </a>
+                        <a href={"#"} className={"ms-2 tabs__profile__container__header__view-all"}>
+                            View All
+                        </a>
+                    </div>
+                </div>
+                <div className={"px-4"}>
+                    <div style={{height: '384px'}}>
+                        <Song hasPlusAndDownload hasBorder href={"#"} img={"http://volna.volkovdesign.com/img/covers/cover.svg"}
+                              singer={"Got What I Got"} isLive={100} name={"Jason Aldean"}/>
+                        <Song hasPlusAndDownload hasBorder href={"#"} img={"http://volna.volkovdesign.com/img/covers/cover.svg"}
+                              singer={"Got What I Got"} isLive={100} name={"Jason Aldean"}/>
+                        <Song hasPlusAndDownload hasBorder href={"#"} img={"http://volna.volkovdesign.com/img/covers/cover.svg"}
+                              singer={"Got What I Got"} isLive={100} name={"Jason Aldean"}/>
+                        <Song hasPlusAndDownload hasBorder href={"#"} img={"http://volna.volkovdesign.com/img/covers/cover.svg"}
+                              singer={"Got What I Got"} isLive={100} name={"Jason Aldean"}/>
+                        <Song hasPlusAndDownload hasBorder={false} href={"#"} img={"http://volna.volkovdesign.com/img/covers/cover.svg"}
+                              singer={"Got What I Got"} isLive={100} name={"Jason Aldean"}/>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 }
